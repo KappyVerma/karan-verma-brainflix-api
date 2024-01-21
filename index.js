54cd48b5f0ff917628725b92e89ed659ce97cb12
Karan Verma <karranverma@gmail.com>
2024-01-20 21:59:40 -0800

    fixid issues

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
+// Load environment variables from .env file
 require("dotenv").config();
 
+// Import required modules
 const express = require("express");
 const app = express();
-const apiUrl = `${process.env.BASE_URL}:${process.env.PORT}`;
-const PORT = process.env.PORT || 5050;
 const cors = require("cors");
-app.use(express.static("public/images"));
-app.use(middleware);
+
+// Define the API URL using environment variables
+const apiUrl = `${process.env.BASE_URL}:${process.env.PORT}`;
+
+// Set the PORT for the server
+const PORT = process.env.PORT || 5050;
+
+// Enable CORS for all routes
 app.use(cors());
+
+// Serve static images from the "public/images" directory
+app.use(express.static("public/images"));
+
+// Custom middleware function for logging
+app.use(middleware);
+
+// Parse incoming JSON requests
 app.use(express.json());
 
-function middleware(req, res, next) {
+// Custom middleware function for logging date and URL
+function middleware(req, _res, next) {
   console.log(new Date().toLocaleDateString());
   console.log(req.url);
   next();
 }
 
+// Import and use video-related routes
 const videoRoutes = require("./routes/videos");
-
 app.use("/videos", videoRoutes);
 
-app.use("/", (req, res) => {
+// Default route for all other paths
+app.use("/", (_req, res) => {
   res.send("you are hitting the server");
 });
 
+// Start the server and listen on the specified PORT
 app.listen(PORT, () => {
   console.log(`listening on post ${apiUrl}`);
 });
